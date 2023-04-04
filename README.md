ab44d6179130b70c8cd4d5179879bc3b2a32f16b
Rhea Sharma <77135078+Rhea-Sharma@users.noreply.github.com>
2023-04-04 18:05:13 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+# Rhea-Sharma.github.io
+This is my personal website made by yours truly check it out, hope like it :)!
---
+++ assets/scripts/scripts.js
@@ -0,0 +1,114 @@
+// Typewriter created by Tameem Safi.
+// source: https://github.com/tameemsafi/typewriterjs
+
+var app = document.getElementById('app');
+
+var typewriter = new Typewriter(app, {
+    loop: true,
+    delay:50,
+    deleteSpeed:5
+});
+
+typewriter.typeString('hey there!')
+    .pauseFor(1000)
+    .deleteAll()
+    .pauseFor(500)
+    .typeString('nice to meet you.')
+    .pauseFor(1000)
+    .deleteAll()
+    .pauseFor(500)
+    .typeString("my name is rhea.")
+    .pauseFor(1000)
+    .deleteAll()
+    .pauseFor(500)
+    .typeString("i'm a student at wilfrid laurier (go hawks!).")
+    .pauseFor(1000)
+    .deleteAll()
+    .pauseFor(500)
+    .typeString("i'm studying computer science.")
+    .pauseFor(1000)
+    .deleteAll()
+    .pauseFor(500)
+    .typeString("i'm also interested in design.")
+    .pauseFor(1000)
+    .deleteAll()
+    .pauseFor(500)
+    .typeString("big fan of iced matcha lattes.")
+    .pauseFor(1000)
+    .deleteAll()
+    .pauseFor(500)
+    .typeString("i also run a lifestyle account.")
+    .pauseFor(1000)
+    .deleteAll()
+    .pauseFor(500)
+    .typeString("thank you for visting!")
+    .pauseFor(1000)
+    .deleteAll()
+    .start();
+
+// When the user scrolls down 20px from the top of the document, slide down the navbar
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 700 || document.documentElement.scrollTop > 700) {
+    document.getElementById("nav").style.top = "0";
+  } else {
+    document.getElementById("nav").style.top = "-70px";
+  }
+}
+
+
+// $(function() {
+//   $('a[href*=#]').on('click', function(e) {
+//     e.preventDefault();
+//     $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+//   });
+// });
+
+//filter buttons
+//source : https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_filter_elements
+
+filterSelection("all")
+function filterSelection(c) {
+  var x, i;
+  x = document.getElementsByClassName("each-project");
+  if (c == "all") c = "";
+  for (i = 0; i < x.length; i++) {
+    w3RemoveClass(x[i], "show");
+    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+  }
+}
+
+function w3AddClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    console.log(element.className, arr2[i]);
+    if (arr1.indexOf(arr2[i]) == -1) {element.className += " " + arr2[i];}
+  }
+}
+
+function w3RemoveClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    while (arr1.indexOf(arr2[i]) > -1) {
+      arr1.splice(arr1.indexOf(arr2[i]), 1);
+    }
+  }
+  element.className = arr1.join(" ");
+}
+
+// Add active class to the current button (highlight it)
+var btnContainer = document.getElementById("button-section");
+var btns = btnContainer.getElementsByClassName("filter-button");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function(){
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+    console.log(current);
+  });
+}
